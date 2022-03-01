560f317afa155b882c953cf297e2a12099181be8
Vic Kalchev <vic@pivotup.co>
2022-02-28 22:40:08 -0500

    Version 0.1 of the component.

--- index.js
+++ index.js
@@ -2,12 +2,29 @@ const postComment = () => {
     const nameInput = document.getElementById('nameInput').value;
     const commentInput = document.getElementById('commentInput').value;
     
-    let postElement = document.createElement('div');
-    postElement.classList.add('commentWrap');
-    let comment = document.createTextNode(commentInput);
-    postElement.appendChild(comment);
-    return document.getElementById('postedComment').appendChild(postElement);
+    const createListItem = () => {
+
+    let postListItem = document.createElement('li');
+    postListItem.textContent = commentInput;
+    return postListItem;
+    }
+
+    const postList = document.getElementById('postList');
+    return postList.insertBefore(createListItem(commentInput), postList.children[0]); 
 }
 
 const postBtn = document.getElementById('postBtn');
-postBtn.addEventListener("click", postComment);
+postBtn.addEventListener("click", emptyInputTest = () => {
+
+    // if (postComment.length > 0)
+        return postComment();
+});
+
+const clearInput = () => {
+    const inputs = document.querySelectorAll('#nameInput, #commentInput');
+
+    return inputs.forEach(input => {input.value=''});
+}
+
+postBtn.addEventListener("click", clearInput);
+
